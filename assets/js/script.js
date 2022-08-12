aa5b4e24680ee2d3e74c534659a846cac2b10bce
escowin <edwin@escowinart.com>
2022-08-12 15:26:11 -0500

    js | api url test

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// api
+const apiKey = "DEMO_KEY";
+const apiUrl = `https://api.nasa.gov/DONKI/CME?api_key=${apiKey}`;
+
+var getWeather = function() {
+    apiUrl;
+    console.log(apiUrl);
+};
+
+getWeather();
