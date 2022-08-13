d1930ed3572e37a586fa0d43aa9c15153879f059
escowin <edwin@escowinart.com>
2022-08-12 22:54:56 -0500

    npm preamble

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,31 @@
-// api
+// time
+let currentDate = new Date();
+let currentDay = currentDate.getDate();
+let currentMonth = currentDate.getMonth() + 1;
+let currentYear = currentDate.getFullYear();
+
+let endDate = `${currentYear}-${currentMonth}-${currentDay}`
+console.log(endDate)
+
+
+console.log(startDay);
+
+
+// api.cme
 const apiKey = "DEMO_KEY";
-const apiUrl = `https://api.nasa.gov/DONKI/CME?api_key=${apiKey}`;
+const apiUrl = `https://api.nasa.gov/DONKI/CME?endDate=${endDate}&api_key=${apiKey}`;
 
 var getWeather = function() {
-    apiUrl;
-    console.log(apiUrl);
+    fetch(apiUrl)
+     .then(function (response){
+        return response.json();
+     })
+     .then(function (data) {
+        console.log(data);
+     });
 };
+
+
+
 
 getWeather();
