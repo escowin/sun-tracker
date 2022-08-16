9c4cb2bf94eb5cb4acbe4c79a35d36d917878d88
escowin <edwin@escowinart.com>
2022-08-15 23:07:27 -0500

    js | + moment.js for display & api dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,24 @@
 // dom
 const currentDateEl = document.querySelector("#current-date");
 
-// logic.get-time
-let currentDate = new Date();
-let currentDay = currentDate.getDate();
-let currentMonth = currentDate.getMonth() + 1;
-let currentYear = currentDate.getFullYear();
-
-let endDate = `${currentYear}-${currentMonth}-${currentDay}`
-console.log(endDate)
+// logic.API-time
+var startDate = moment().subtract(7, "days").format("YYYY-MM-DD");
+console.log(startDate);
+var endDate = moment().format("YYYY-MM-DD");
+console.log(endDate);
 
 // logic.display-time
+var currentDate = moment().format("MMMM Do");
 currentDateEl.innerHTML = currentDate;
-console.log(currentDateEl);
+
+
+
 
 
 // api.cme
 const apiKey = "DEMO_KEY";
-const apiUrl = `https://api.nasa.gov/DONKI/CME?endDate=${endDate}&api_key=${apiKey}`;
+const apiUrl = `https://api.nasa.gov/DONKI/CME?startDate=${startDate}&endDate=${endDate}&api_key=${apiKey}`;
+console.log(apiUrl);
 
 var getWeather = function() {
     fetch(apiUrl)
