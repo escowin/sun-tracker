db682605f3d27a2465e05e01d778f7a5e835d91f
escowin <edwin@escowinart.com>
2022-08-15 23:24:32 -0500

    js | reorganized variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,17 @@
-// dom
+// data.dom
 const currentDateEl = document.querySelector("#current-date");
 
-// logic.API-time
-var startDate = moment().subtract(7, "days").format("YYYY-MM-DD");
-console.log(startDate);
-var endDate = moment().format("YYYY-MM-DD");
-console.log(endDate);
-
-// logic.display-time
-var currentDate = moment().format("MMMM Do");
-currentDateEl.innerHTML = currentDate;
-
-
-
-
-
-// api.cme
+// api.coronal-mass-ejection
 const apiKey = "DEMO_KEY";
+const startDate = moment().subtract(7, "days").format("YYYY-MM-DD");
+const endDate = moment().format("YYYY-MM-DD");
 const apiUrl = `https://api.nasa.gov/DONKI/CME?startDate=${startDate}&endDate=${endDate}&api_key=${apiKey}`;
-console.log(apiUrl);
 
+// logic.displays current time
+var currentDate = moment().format("MMMM Do");
+currentDateEl.textContent = currentDate;
+
+// logic
 var getWeather = function() {
     fetch(apiUrl)
      .then(function (response){
