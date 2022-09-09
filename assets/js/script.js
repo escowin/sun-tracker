8316658870f956fa238b04d837a9d51ea9c03da1
escowin <edwin@escowinart.com>
2022-09-08 20:56:44 -0500

    refactored api logic & current time functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,35 @@
-// data.dom
-const currentDateEl = document.querySelector("#current-date");
+// logic.display copyright year
+const copyrightYear = function() {
+   let year = new Date().getFullYear();
+   const copyrightYearEl = document.querySelector("#copyright-year");
+   copyrightYearEl.textContent = year;
+};
 
-// api.coronal-mass-ejection
-// to-do | use the real api key but keep it hidden
+// logic.display current date
+const currentDate = function() {
+   let date = moment().format("MMMM Do");
+   const currentDateEl = document.querySelector("#current-date");
+   currentDateEl.textContent = date;
+};
+
+// logic.api
 const apiKey = "DEMO_KEY";
+const startDate = moment().subtract(7, "days").format("YYYY-MM-DD");
 const endDate = moment().format("YYYY-MM-DD");
 
-// logic.displays current time
-var currentDate = moment().format("MMMM Do");
-currentDateEl.textContent = currentDate;
-
-// fetches the array of recent coronal mass ejections
 const getCoronalMassEjections = function() {
-   const startDate = moment().subtract(7, "days").format("YYYY-MM-DD");
    const apiUrl = `https://api.nasa.gov/DONKI/CME?startDate=${startDate}&endDate=${endDate}&api_key=${apiKey}`;
 
-   // fetching the array of recent solar flares
+   // fetching the array of recent coronal mass ejections
    fetch(apiUrl).then(function(response) {
       // method formats the response as json. returns a promise. the then() method captures the actual data
       response.json().then(function(data) {
          console.log(data);
       });
    });
-}
+};
 
 const getSolarFlares = function() {
-   const startDate = moment().subtract(7, "days").format("YYYY-MM-DD");
    const apiUrl = `https://api.nasa.gov/DONKI/FLR?startDate=${startDate}&endDate=${endDate}&api_key=${apiKey}`;
 
    // fetching the array of recent solar flares
@@ -35,8 +39,10 @@ const getSolarFlares = function() {
          console.log(data);
       });
    });
-}
+};
 
 // calls. to-do | maybe combine both calls into getSolarActivity() that takes in different parameters (CME, FLR) to streamline the functions bc they're awfully similar atm
+copyrightYear();
+currentDate();
 getCoronalMassEjections();
 getSolarFlares();
