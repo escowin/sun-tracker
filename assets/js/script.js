7f2fa28a4cef07ec3464bcebd40dc85b7019948e
escowin <edwin@escowinart.com>
2023-08-02 10:32:17 -0500

    streamlined fetch request functions with es6 syntax & object destructuring

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,86 +76,6 @@ function currentDate() {
    `);
 }
 
-// logic.display days of the week
-// const forecast = function () {
-//   const day2El = document.querySelector("#day-2");
-//   const day3El = document.querySelector("#day-3");
-//   const day4El = document.querySelector("#day-4");
-//   const day5El = document.querySelector("#day-5");
-
-//   const tomorrow = moment().add(1, "days");
-//   day2El.textContent = tomorrow.format("dddd");
-
-//   const day3 = moment().add(2, "days");
-//   day3El.textContent = day3.format("dddd");
-
-//   const day4 = moment().add(3, "days");
-//   day4El.textContent = day4.format("dddd");
-
-//   const day5 = moment().add(4, "days");
-//   day5El.textContent = day5.format("dddd");
-// };
-
-// logic.display api data
-const displayCoronalMassEjections = function (CME) {
-  const cmeTimeEl = document.querySelector("#cme-time");
-  const cmeLatitudeEl = document.querySelector("#cme-latitude");
-  const cmeLongitudeEl = document.querySelector("#cme-longitude");
-  const cmeAngleEl = document.querySelector("#cme-angle");
-  const cmeSpeedEl = document.querySelector("#cme-speed");
-  const cmeTypeEl = document.querySelector("#cme-type");
-  const cmeNoteEl = document.querySelector("#cme-note");
-
-  // get latest coronal mass ejection data
-  latestCME = CME[CME.length - 1];
-
-  // reformat time
-  const startTime = dayjs(latestCME.startTime).format("dddd, MMMM Do h:mm a");
-
-  // display data
-  cmeTimeEl.textContent = startTime;
-  cmeLatitudeEl.textContent = `${latestCME.cmeAnalyses[0].latitude}°`;
-  cmeLongitudeEl.textContent = `${latestCME.cmeAnalyses[0].longitude}°`;
-  cmeAngleEl.textContent = `${latestCME.cmeAnalyses[0].halfAngle}°`;
-  cmeSpeedEl.textContent = `${latestCME.cmeAnalyses[0].speed} kph`;
-  cmeTypeEl.textContent = latestCME.cmeAnalyses[0].type;
-  cmeNoteEl.textContent = latestCME.note;
-};
-
-const displaySolarFlares = function (FLR) {
-  const flrDateEl = document.querySelector("#flr-date");
-  const flrDurationEl = document.querySelector("#flr-duration");
-  const flrRegionEl = document.querySelector("#flr-region");
-  const flrBeginTimeEl = document.querySelector("#flr-begin");
-  const flrPeakTimeEl = document.querySelector("#flr-peak");
-  const flrEndTimeEl = document.querySelector("#flr-end");
-  const flrLocationEl = document.querySelector("#flr-location");
-  const flrClassEl = document.querySelector("#flr-class");
-
-  // get latest solar flare data
-  const latestFLR = FLR[FLR.length - 1];
-
-  // reformat time
-  const flrDate = dayjs(latestFLR.beginTime).format("dddd, MMMM Do");
-  const beginTime = dayjs(latestFLR.beginTime).format("hh:mm a");
-  const peakTime = dayjs(latestFLR.peakTime).format("hh:mm a");
-  const endTime = dayjs(latestFLR.endTime).format("hh:mm a");
-
-  // calculating the solar flare duration
-  const start = dayjs(latestFLR.beginTime);
-  const end = dayjs(latestFLR.endTime);
-  const duration = end.diff(start, "minute");
-
-  flrDateEl.textContent = flrDate;
-  flrDurationEl.textContent = `${duration} minutes`;
-  flrRegionEl.textContent = latestFLR.activeRegionNum;
-  flrBeginTimeEl.textContent = beginTime;
-  flrPeakTimeEl.textContent = peakTime;
-  flrEndTimeEl.textContent = endTime;
-  flrLocationEl.textContent = latestFLR.sourceLocation;
-  flrClassEl.textContent = latestFLR.classType;
-};
-
 // logic.api set-up
 // to-do : hide real api key
 function apiCalls() {
@@ -166,24 +86,48 @@ function apiCalls() {
   getSolarFlares(apiKey, apiStart, apiEnd);
 }
 
+// makes fetch requests to get CME data from NASA DONKI API
 function getCoronalMassEjections(key, start, end) {
   const apiUrl = `https://api.nasa.gov/DONKI/CME?startDate=${start}&endDate=${end}&api_key=${key}`;
 
-  // fetching the array of recent coronal mass ejections
-  fetch(apiUrl).then((response) => {
-    // method formats the response as json. returns a promise. the then() method captures the actual data
-    response.json().then((data) => displayCoronalMassEjections(data));
-  });
+  // fetch request response is formmated as a JSON object that is then passed as an argument
+  fetch(apiUrl).then((res) =>
+    res.json().then((data) => displayCoronalMassEjections(data))
+  );
 }
 
+// makes fetch requests to get FLR data from NASA DONKI API
 function getSolarFlares(key, start, end) {
   const apiUrl = `https://api.nasa.gov/DONKI/FLR?startDate=${start}&endDate=${end}&api_key=${key}`;
 
-  // fetching the array of recent solar flares
-  fetch(apiUrl).then((response) => {
-    // method formats the response as json. returns a promise. the then() method captures the actual data
-    response.json().then((data) => displaySolarFlares(data));
-  });
+  // fetch request response is formmated as a JSON object that is then passed as an argument
+  fetch(apiUrl).then((res) =>
+    res.json().then((data) => displaySolarFlares(data))
+  );
+}
+
+function displayCoronalMassEjections(CME) {
+  // selects last object in CME array to get the most recent data
+  const latestCME = CME[CME.length - 1];
+
+  // retrieves relevant variables through object destructuring
+  const { startTime, note, sourceLocation, cmeAnalyses } = latestCME;
+  const { latitude, longitude, halfAngle, speed, type } = cmeAnalyses[0];
+}
+
+function displaySolarFlares(FLR) {
+  // get latest solar flare data
+  const latestFLR = FLR[FLR.length - 1];
+
+  // retrieves relevant variables through object destructuring
+  const {
+    beginTime,
+    peakTime,
+    endTime,
+    activeRegionNum,
+    sourceLocation,
+    classType,
+  } = latestFLR;
 }
 
 // calls
--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,9 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 module.exports = {
   // PWA configuraton settings
-  entry:{
-     app: "./assets/js/script.js",
-    },
+  entry: {
+    app: "./assets/js/script.js",
+  },
   output: {
     // saves bundle file to `dist/`
     path: path.resolve(__dirname, "dist"),
@@ -42,26 +42,29 @@ module.exports = {
       dayjs: "dayjs",
     }),
     new BundleAnalyzerPlugin({
-      // outputs `report.html` in `dist`. "disable" stops report generation
-      analyzerMode: "static",
+      // "static" generates `report.html`. "disable" stops report generation
+      analyzerMode: "disable",
     }),
     new WebpackPwaManifest({
       // `manifest.json` object key-values
       publicPath: "./",
       name: "Sun tracker",
       short_name: "sun-tracker",
-      description: "tracks Sun activity with math formulas & data fetched from NASA's DONKI API",
+      description:
+        "tracks Sun activity with math formulas & data fetched from NASA's DONKI API",
       start_url: "../index.html",
-      background_color:  "#860e0e",
+      background_color: "#860e0e",
       theme_color: "#860e0e", // nee bf9732
       fingerprints: false,
       inject: false,
-      icons: [{
-        src: path.resolve("assets/images/icons/icon-512x512.png"),
-        sizes: [96, 128, 192, 256, 384, 512],
-        destination: path.join("assets", "icons")
-      }]
-    })
+      icons: [
+        {
+          src: path.resolve("assets/images/icons/icon-512x512.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        },
+      ],
+    }),
   ],
   mode: "development", // webpack runs during development
   devServer: { static: "./" }, // non-webpack content loads from root in development environment
