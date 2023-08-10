b832595502716a5ccc874d0a7d2147d7cfa073cb
escowin <edwin@escowinart.com>
2023-08-10 16:04:01 -0500

    reconfigured object & functions into OOP Sun class

--- assets/js/sunData.js
+++ assets/js/sunData.js
@@ -1,14 +1,51 @@
-const { fluctuate } = require("./helper");
-const { utcNow, perihelion } = require("./time")
+const { utcNow, perihelion, duration } = require("./time");
 
-const sun = {
-  temp: fluctuate(5772),
-  spectral: "G2V",
-  metallicity: "Z = 0.0122",
-  luminosity: "L⊙ = 4πkI⊙A2",
-  distance: currentDistance(utcNow, perihelion),
-  lm: () =>
-    `${(stats.distance / 17987547.48).toLocaleString("en-US")} light minutes`,
-};
+class Sun {
+  constructor() {
+    this.distance = this.currentDistance(utcNow, perihelion);
+    this.luminosity = "L☉ = 4πkI☉A²";
+    this.metallicity = "Z = 0.0122";
+    this.spectral = "G2V";
+    this.temp = this.fluctuate(5772);
+  }
 
-module.exports = { sun };
+  luminosity() { 
+    const A = this.distance
+    // k = solar constant (avg. 1.3608 ± 0.0005  kW/m)
+    // I☉ = solar irradiance
+
+    // luminosity formula L☉ = 4πkI☉A²
+    const result = (4 * Math.PI() * k);
+  }
+
+  fluctuate(num) {
+    // sets a random high low range sun temp to then randomly return a number from
+    const high = num + Math.round(Math.random() * 3000);
+    const low = num + Math.round((Math.random() - 0.5) * 1000);
+    const result = Math.round(low + Math.random() * (high - low));
+    return result;
+  }
+
+  currentDistance(now, perihelion) {
+    const totalDays = duration(perihelion, now, "day");
+    // time, semi-major axis, eccentricity
+    const t = (totalDays * 365.25) / 360;
+    const a = 149600000;
+    const e = 0.017;
+    // earth-sun distance equation
+    const orbit = (a * (1 - e * e)) / (1 + e * Math.cos(t));
+
+    return orbit;
+  }
+
+  duration(start, end, unit) {
+    return duration(start, end, unit)
+  };
+
+  lightMinutes() {
+    const lightMinutes = (this.distance / 17987547.48).toLocaleString("en-US");
+    return `${lightMinutes} light minutes`
+  }
+}
+
+module.exports = Sun;
