950b95b0c8cd2811e3b134c0bcc2de7f51d38f7d
escowin <edwin@escowinart.com>
2023-08-29 00:28:45 -0500

    reconfigured displayData to rudimentary Display class; simplified script to serve instantiated object

--- src/js/script.js
+++ src/js/script.js
@@ -2,18 +2,17 @@ import "../css/styles.css";
 const { displayData } = require("./utils/displayData");
 const { year } = require("./utils/time");
 const { mockFLR, mockCME } = require("./mock/data");
-const Memory = require("./lib/Memory")
-const memory = new Memory();
+const Display = require("./lib/Display")
+const display = new Display();
 
-memory.openDatabase();
-// console.log(memory)
-
-// javascript functions handle data before the dom
 console.log(`
    \u00A9 ${year} Edwin M. Escobar
    https://github.com/escowin/sun-tracker
    `);
 
+display.openDatabase();
+display.displayData();
+// console.log(display)
 
 // calls
 // apiCalls();
