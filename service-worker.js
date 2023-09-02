f012862be0e1658858cfbb6a74a1c07194429b46
escowin <edwin@escowinart.com>
2023-09-02 15:33:07 -0500

    generated 2.7.1 build: included sw & manifest in cache; adjusted media query styles

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,14 @@
 // service worker enables offline functionality
 // development note: clear storage in devTools whenever changes are made to service worker
 const APP_PREFIX = "SunTracker-";
-const VERSION = "2.7.0";
+const VERSION = "2.7.1";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 // cache of essential files
 const FILES_TO_CACHE = [
   "./index.html",
+  "./service-worker.js",
+  "./dist/manifest.json",
   "./dist/app.bundle.js",
   "./dist/assets/img/background.jpg",
 ];
